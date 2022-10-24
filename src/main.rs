778b02ff2d08865db263eca0247785ba94521dd2
Sangchual CHA <scha@swidch.com>
2022-10-24 19:48:19 +0100

    get line index from enumerate()

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@ Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of papers?";
-    let mut line_num = 0;
 
-    for line in quote.lines() {
-        line_num += 1;
+    for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
-            println!("{} {}", line_num, line);
+            println!("{} {}", i+1, line);
         }
     }
 }
