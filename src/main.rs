39a01dc409e530040d8edcf42a96a8add3ea19c5
Sangchual CHA <scha@swidch.com>
2022-10-24 19:46:46 +0100

    show line num with an index variable

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of papers?";
+    let mut line_num = 0;
 
     for line in quote.lines() {
+        line_num += 1;
         if line.contains(search_term) {
-            println!("{}", line);
+            println!("{} {}", line_num, line);
         }
     }
 }
