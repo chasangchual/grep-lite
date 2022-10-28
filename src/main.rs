bb369d2b0d15df9dd7fbe3e9d89f50673618f447
Sangchual CHA <sangchual.cha@hotmail.com>
2022-10-28 19:35:54 +0100

    show matched line

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ feverishly turned--in search of what?
 It is the same with books.
 What do we seek
 through millions of papers?";
-    let mut tags: Vec<usize> = Vec::new();
-    let mut ctx: Vec<Vec<(usize, String)>> = Vec::new();
+    let mut tags: Vec<usize> = Vec::new(); // list of line numbers of the matched
+    let mut ctx: Vec<Vec<(bool, usize, String)>> = Vec::new();
 
     for (i, line) in haystack.lines().enumerate() {
         if line.contains(needle) {
@@ -25,19 +25,29 @@ through millions of papers?";
         return;
     }
 
+    /*
+        haystack is a list of strings
+
+        for (i, line) in haystack.lines().enumerate() {
+        : 'line' is declared as &str
+
+        for (i, &line) in haystack.lines().enumerate() {
+        : 'line' is declared as str
+     */
+
     for (i, line) in haystack.lines().enumerate() {
         for(j, tag) in tags.iter().enumerate() {
             let lower_bound = tag.saturating_sub(context_lines);
             let upper_bound = tag + context_lines;
             if i >= lower_bound && i <= upper_bound {
-                ctx[j].push((i, String::from(line)));
+                ctx[j].push(((i==*tag), i, String::from(line)));
             }
         }
     }
 
     for v in ctx {
-        for (i, line) in v {
-            println!("{} {}", i+1, line);
+        for (matched, i, line) in v {
+            println!("{} {} {:?}", if matched {'m'} else {' '}, i+1, line);
         }
     }
 }
