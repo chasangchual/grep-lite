a691c60103e5f24c428baa79d6ae72dced9922f7
Sangchual CHA <sangchual.cha@hotmail.com>
2022-10-31 08:17:54 +0000

    find a containing string with regular expression

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,20 @@
 use regex::Regex;
 
 fn main() {
-    let context_lines = 2;
-    let needle = "oo";
-    let haystack = "\
-Every face, every shop,
-bedroom window, public-house, and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of papers?";
-    let mut tags: Vec<usize> = Vec::new(); // list of line numbers of the matched
-    let mut ctx: Vec<Vec<(bool, usize, String)>> = Vec::new(); // list of the matched/before/after lines
+    let search_term = "picture";
+    let quote = "\
+Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of papers?";
 
-    for (i, line) in haystack.lines().enumerate() {
-        if line.contains(needle) {
-            tags.push(i); // push matched line number
+    let re = Regex::new(search_term).unwrap(); // unwrap(), if any error condition occurres while running new(), it will terminate
 
-            let v = Vec::with_capacity(2*context_lines + 1); // space the space for the lines
-            ctx.push(v);
-        }
-    }
 
-    if tags.is_empty() {
-        return;
-    }
-
-    /*
-        haystack is a list of strings
-
-        for (i, line) in haystack.lines().enumerate() {
-        : 'line' is declared as &str
-
-        for (i, &line) in haystack.lines().enumerate() {
-        : 'line' is declared as str
-     */
-
-    for (i, line) in haystack.lines().enumerate() {
-        for(j, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(context_lines);
-            let upper_bound = tag + context_lines;
-            if i >= lower_bound && i <= upper_bound {
-                ctx[j].push(((i==*tag), i, String::from(line))); // (i==*tag) adds matched or not status
-            }
-        }
-    }
-
-    for v in ctx {
-        for (matched, i, line) in v {
-            println!("{} {} {:?}", if matched {'m'} else {' '}, i+1, line);
+    for (_i, line) in quote.lines().enumerate() {
+        let contains_substring = re.find(line);
+        match contains_substring {
+            Some(matched) => println!("{} \n - {:?}", line, matched), // 'matched' prints Match { text: "dark square is a picture feverishly turned--in search of what?", start: 17, end: 24 }
+            None => (),
         }
     }
 }
