d64874cf75889f7ff9e37ccb782bc8eb0f33f61a
Sangchual CHA <sangchual.cha@hotmail.com>
2022-11-04 09:11:02 +0000

    take parameters from the command line and use regular express pattern

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use regex::Regex;
+use clap::{Arg, arg, Command, ArgAction};
 
 fn main() {
-    let search_term = "picture";
+    let matches = Command::new("grep-lite")
+        .version("0.1")
+        .author("Sangchual CHA <sangchual.cha@gmail.com>")
+        .about("grep lite version in Rus")
+        .arg(arg!(--pattern <regexpr>)
+            .short('p')
+            .help("the regular expression pattern to search for")
+            .required(true))
+        .arg(Arg::new("verbose").short('v').long("verbose").required(false).action(ArgAction::SetTrue))
+        .get_matches();
+
+    let search_pattern = matches.get_one::<String>("pattern").unwrap();
     let quote = "\
 Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of papers?";
 
-    let re = Regex::new(search_term).unwrap(); // unwrap(), if any error condition occurres while running new(), it will terminate
+    let re = Regex::new(search_pattern.as_str()).unwrap(); // unwrap(), if any error condition occurres while running new(), it will terminate
 
 
     for (_i, line) in quote.lines().enumerate() {
